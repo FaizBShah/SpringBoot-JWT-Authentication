fb845154098c518fb2d846ae7e9ce09e512e2f13
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2022-12-30 19:01:05 +0530

    First commit

---
+++ src/main/java/com/example/springjwtauthentication/SpringJwtAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjwtauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtAuthenticationApplication.class, args);
+	}
+
+}
